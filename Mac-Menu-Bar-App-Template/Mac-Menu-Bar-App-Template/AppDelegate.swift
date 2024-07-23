0c2d58947168855479d495ff0f4bf643930dbd47
Eden <levine5blue@gmail.com>
2024-07-24 01:43:56 +0800

    [UPDATE] Support ⌘+w shortcut.

--- Mac-Menu-Bar-App-Template/Mac-Menu-Bar-App-Template/AppDelegate.swift
+++ Mac-Menu-Bar-App-Template/Mac-Menu-Bar-App-Template/AppDelegate.swift
@@ -8,11 +8,33 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        populateMainMenu()
+    }
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         MenuBarItemController.shared.setUp()
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
+    }
+}
+
+extension AppDelegate {
+    /// https://github.com/sindresorhus/Settings/issues/40
+    func populateMainMenu() {
+        let mainMenu = NSMenu(title: "Main Menu")
+        let fileMenuItem = mainMenu.addItem(withTitle: "File", action: nil, keyEquivalent: "")
+        let submenu = NSMenu(title: NSLocalizedString("File", comment: "File Menu"))
+
+        let closeWindowItem = NSMenuItem(title: NSLocalizedString("Close Window", comment: "Close Window menu item"),
+                                         action: #selector(NSWindow.performClose(_:)),
+                                         keyEquivalent: "w")
+        submenu.addItem(closeWindowItem)
+
+        mainMenu.setSubmenu(submenu, for: fileMenuItem)
+
+        NSApp.mainMenu = mainMenu
     }
 }
